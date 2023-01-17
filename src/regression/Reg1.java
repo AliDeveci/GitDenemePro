172b90d5c236668b220920a9301b2e6cef695325
onurbapur <onurrbapurr@gmail.com>
2023-01-17 21:12:45 +0300

    onur commit

--- src/regression/Reg1.java
+++ src/regression/Reg1.java
@@ -2,7 +2,6 @@ package regression;
 
 public class Reg1 {
     public static void main(String[] args) {
-        System.out.println("clone edildi. değişiklik yapıldı");
         System.out.println("Reg1 Test suit oluşturuldu.");
     }
 }
